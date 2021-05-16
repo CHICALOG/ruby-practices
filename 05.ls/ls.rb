0769b6933bd0d7c7261c200fcc3d26376f02389b
CHICALOG <66663821+CHICALOG@users.noreply.github.com>
2021-05-16 13:58:58 +0900

    ls コマンドを作る(ruby)_再提出
    詳細は、FBC提出ページへ記載しますが、ls コマンドを作る(ruby)課題の修正を行いましたので、再提出します。
    ご確認お願いします。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,99 +2,112 @@
 # frozen_string_literal: true
 
 require 'etc'
+require 'optparse'
 
-def file_names
-  Dir.glob('*').sort
+TAB_WIDTH = 8
+COLUMN_COUNT = 3
+
+FILE_TYPE = {
+  'directory' => 'd',
+  'file' => '-',
+  'link' => 'l'
+}.freeze
+
+LOCAL_PERMISSION = {
+  '0' => '---',
+  '1' => '--x',
+  '2' => '-w-',
+  '3' => '-wx',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx'
+}.freeze
+
+def main
+  opt = ARGV.getopts('a', 'r', 'l')
+
+  file_names = if opt['a']
+                 Dir.glob('*', File::FNM_DOTMATCH).sort
+               else
+                 Dir.glob('*').sort
+               end
+
+  file_names = file_names.reverse if opt['r']
+
+  if opt['l']
+    ls_command_with_l_option(file_names)
+  else
+    ls_command_without_l_option(file_names)
+  end
 end
 
-def file_mode_permission(file)
-  eight_ary = File::Stat.new(file).mode.to_s(8)
-  alignment = format('%06d', eight_ary)
+def ls_command_without_l_option(file_names)
+  longest_file_name = file_names.max_by(&:size).size
+  width = TAB_WIDTH * (longest_file_name / TAB_WIDTH.to_f).ceil
+
+  formatted_file_names = file_names.map { |file_name| file_name.ljust(width) }
+
+  row_count = (file_names.size.to_f / COLUMN_COUNT).ceil
+  nested_file_names = formatted_file_names.each_slice(row_count).to_a
+
+  nested_file_names = nested_file_names.map { |inner_file_names| inner_file_names.values_at(0...row_count) }
+  nested_file_names.transpose.each do |file|
+    puts file.join(' ')
+  end
+end
+
+def ls_command_with_l_option(file_names)
+  total = 0
+  file_names.each do |file_name|
+    file_stat = File::Stat.new(file_name)
+    total += file_stat.blocks
+  end
+  puts "total #{total}"
+
+  file_names.each do |file_name|
+    file_stat = File::Stat.new(file_name)
+    details = "#{type_and_permission(file_name)} " \
+              "#{file_stat_nlink(file_name)} " \
+              "#{Etc.getpwuid(file_stat.uid).name} " \
+              "#{Etc.getgrgid(file_stat.gid).name} " \
+              "#{format_filesize(file_name)} " \
+              "#{file_stat.mtime.strftime('%m %e %H:%M')} " \
+              "#{file_name} "
+    puts details
+  end
+end
+
+def local_permission(file)
+  octal_mode = File::Stat.new(file).mode.to_s(8)
+  octal_mode[-3..]
 end
 
 def file_type(file)
-  details_filename = File::Stat.new(file)
-  if details_filename.ftype == 'directory'
-    'd'
-  elsif details_filename.ftype == 'file'
-    '-'
-  else
-    details_filename.ftype == 'link'
-    'l'
-  end
+  type = File::Stat.new(file).ftype
+  FILE_TYPE[type]
 end
 
-def file_eachpermission_index_three(file)
-  file_eachpermission_number = file_mode_permission(file)[3]
-  case file_mode_permission(file)[3]
-  when '0' then '---'
-  when '1' then '--x'
-  when '3' then '-wx'
-  when '4' then 'r--'
-  when '5' then 'r-x'
-  when '6' then 'rw-'
-  when '7' then 'rwx'
-  end
+def permission_by_index(file, index)
+  type = local_permission(file)[index]
+  LOCAL_PERMISSION[type]
 end
 
-def file_eachpermission_index_four(file)
-  file_eachpermission_number = file_mode_permission(file)[4]
-  case file_mode_permission(file)[4]
-  when '0' then '---'
-  when '1' then '--x'
-  when '3' then '-wx'
-  when '4' then 'r--'
-  when '5' then 'r-x'
-  when '6' then 'rw-'
-  when '7' then 'rwx'
-  end
+def type_and_permission(file)
+  file_type(file) +
+    permission_by_index(file, 0) +
+    permission_by_index(file, 1) +
+    permission_by_index(file, 2)
 end
 
-def file_eachpermission_index_five(file)
-  file_eachpermission_number = file_mode_permission(file)[5]
-  case file_mode_permission(file)[5]
-  when '0' then '---'
-  when '1' then '--x'
-  when '3' then '-wx'
-  when '4' then 'r--'
-  when '5' then 'r-x'
-  when '6' then 'rw-'
-  when '7' then 'rwx'
-  end
+def file_stat_nlink(file)
+  file_nlink = File::Stat.new(file).nlink
+  format('%2d', file_nlink)
 end
 
-def file_eachpermission(file)
-  modestring = [file_type(file)] + [file_eachpermission_index_three(file)] + [file_eachpermission_index_four(file)] + [file_eachpermission_index_five(file)]
-  modestring.join
+def format_filesize(file)
+  file_size = File::Stat.new(file).size
+  format('%4d', file_size)
 end
 
-total_blocks = File::Stat.new($0).blocks
-total = ['total'] + [' '] + [total_blocks]
-puts total.join
-
-file_names.each do |f|
-  details_filename = File::Stat.new(f) # 常に最大桁数(最大の桁を撮ってくる(今回の場合は4桁))に合わせて表示する
-  details = [file_eachpermission(f)] + [details_filename.nlink] + [Etc.getpwuid(details_filename.uid).name] + [Etc.getgrgid(details_filename.gid).name] + [details_filename.size] + [details_filename.mtime.strftime('%m %e %H:%M')] + [f]
-  puts details.join('    ')
-end
-
-# TAB_WIDTH = 8
-# COLUMN_COUNT = 3
-
-# file_names = Dir.glob("*").sort
-# file_names = Dir.glob("*").sort.reverse #-r
-# file_names = Dir.glob("*", File::FNM_DOTMATCH).sort #-a
-# file_names = Dir.glob("*", File::FNM_DOTMATCH).sort.reverse #-ar
-
-# longest_file_name = file_names.max_by(&:size).size
-# width = TAB_WIDTH * (longest_file_name / TAB_WIDTH.to_f).ceil
-
-# formatted_file_names = file_names.map {|file_name| file_name.ljust(width)}
-
-# row_count = (file_names.size.to_f / COLUMN_COUNT).ceil
-# nested_file_names = formatted_file_names.each_slice(row_count).to_a
-
-# nested_file_names = nested_file_names.map {|inner_file_names| inner_file_names.values_at(0...row_count)}
-# nested_file_names.transpose.each do |file|
-#   puts file.join(' ')
-# end
+main
